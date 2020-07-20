f3f8544709fe84e3c02a094695926834ecf3f3e4
Firus Hanov <d00363593@dmail.dixie.edu>
2020-07-20 11:25:26 -0600

    test-commit to github

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(".dropdown-trigger").dropdown();
+
+
+
+
+// ==============================
+// Carousel with featured classes.
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
